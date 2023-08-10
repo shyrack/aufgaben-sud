02bb227bd0f30f86ea908b209c25062960475bbf
dougDimmadome420 <andre010400@web.de>
2023-08-10 13:54:43 +0200

    Typing

--- src/sud/aufgaben/occupationalTherapy/a_man_has_fallen_into_a_river/in_lego_city.java
+++ src/sud/aufgaben/occupationalTherapy/a_man_has_fallen_into_a_river/in_lego_city.java
@@ -1,31 +1,31 @@
 package sud.aufgaben.occupationalTherapy.a_man_has_fallen_into_a_river;
 
 public class in_lego_city {
-    public static void main(String[] args) {
-        simulateCity();
-    }
-
-    private static void simulateCity() {
-
-    }
-
-    private static LegoBauteil getWand(int anzahlSteine) {
-        Legostein stein = new Legostein("rot", 20, 0.1);
-        LegoKomposition wand = new LegoKomposition();
-        for (int i = 0; i < anzahlSteine; i++) {
-            wand.addTeil(stein);
-        }
-        return wand;
-    }
-
-    private static LegoBauteil getRaum(int anzahlMauern) {
-        LegoKomposition raum = new LegoKomposition();
-        for (int i = 0; i < anzahlMauern; i++) {
-            raum.addTeil(getWand());
-        }
-    }
-
-    private static LegoBauteil getHaus(){
-
-    };
+//    public static void main(String[] args) {
+//        simulateCity();
+//    }
+//
+//    private static void simulateCity() {
+//
+//    }
+//
+//    private static LegoBauteil getWand(int anzahlSteine) {
+//        Legostein stein = new Legostein("rot", 20, 0.1);
+//        LegoKomposition wand = new LegoKomposition();
+//        for (int i = 0; i < anzahlSteine; i++) {
+//            wand.addTeil(stein);
+//        }
+//        return wand;
+//    }
+//
+//    private static LegoBauteil getRaum(int anzahlMauern) {
+//        LegoKomposition raum = new LegoKomposition();
+//        for (int i = 0; i < anzahlMauern; i++) {
+//            raum.addTeil(getWand());
+//        }
+//    }
+//
+//    private static LegoBauteil getHaus(){
+//
+//    };
 }
