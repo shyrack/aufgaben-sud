ce53eafaa964028621ffd873ccfb455ccb2077fd
dougDimmadome420 <andre010400@web.de>
2023-08-10 11:57:51 +0200

    Completes Tests

--- src/sud/aufgaben/occupationalTherapy/a_man_has_fallen_into_a_river/TestKomposition.java
+++ src/sud/aufgaben/occupationalTherapy/a_man_has_fallen_into_a_river/TestKomposition.java
@@ -1,34 +1,47 @@
 package sud.aufgaben.occupationalTherapy.a_man_has_fallen_into_a_river;
 
 public class TestKomposition {
+    private static Legostein roterStein = new Legostein("rot", 10, 0.05);
+    private static Legostein blauerStein = new Legostein("blauer", 10, 0.1);
+    private static Legostein goldenerStein = new Legostein("gold", 20, 50);
+    private static LegoKomposition kleineKomposition = new LegoKomposition();
+    private static LegoKomposition großeKomposition = new LegoKomposition();
+
     public static void main(String[] args) throws Exception {
         TestLegostein();
         TestKleineKomposition();
+        TestKompositionMitKomposition();
     }
 
     private static void TestLegostein() throws Exception {
         System.out.println("-------TEST LEGOSTEIN-------");
-        Legostein stein = new Legostein("rot", 5, 0.05);
-        double preis = stein.preis();
-
-        if (preis != 0.05) {
-            throw new Exception("Der Preis des Legosteins ist nicht 0.05€. Tatsaechlicher Preis: " + preis + "€");
-        }
+        assertPreis(roterStein, 0.05);
     }
 
     private static void TestKleineKomposition() throws Exception {
         System.out.println("-------TEST KLEINE KOMPOSITION-------");
-        Legostein stein1 = new Legostein("rot", 10, 0.05);
-        Legostein stein2 = new Legostein("gelb", 5, 0.10);
 
-        LegoKomposition komposition = new LegoKomposition();
-        komposition.addTeil(stein1);
-        komposition.addTeil(stein2);
+        kleineKomposition.addTeil(roterStein);
+        kleineKomposition.addTeil(blauerStein);
 
-        double preis = komposition.preis();
+        assertPreis(kleineKomposition, 0.15);
+    }
 
-        if (preis != 0.15) {
-            throw new Exception("Der Preis des Legosteins ist nicht 0.15€. Tatsaechlicher Preis: " + preis + "€");
+    private static void TestKompositionMitKomposition() throws Exception {
+        System.out.println("-------TEST GROSSE KOMPOSITION-------");
+
+        großeKomposition.addTeil(goldenerStein);
+        großeKomposition.addTeil(blauerStein);
+        großeKomposition.addTeil(kleineKomposition);
+
+        assertPreis(großeKomposition, 50.25);
+    }
+
+    private static void assertPreis(LegoBauteil teil, double expected) throws Exception {
+        if (expected != teil.preis()) {
+            throw new Exception("Price of " + teil.preis() + " EURO does not match the expected price of " + expected + " EURO");
+        } else {
+            System.out.println("Preis des Lego-Bauteils ist " + teil.preis() + " EURO");
         }
     }
 }
